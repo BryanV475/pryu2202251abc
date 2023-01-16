0e18760391ff87294d9eedfeb838f83999f0536e
BryanV475 <andresvegabalcarcel.bva@gmail.com>
2023-01-16 18:36:02 -0500

    Estaturas Finish

--- estaturas.py
+++ estaturas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 def searchNull(data):
@@ -13,8 +14,119 @@ def getNullColumns(data_for_read):
         filter = data_for_read[column_name].isnull()
         if(searchNull(filter)):
             yield column_name, filter
+            
+def getUtilRows(data):
+    for  n in range(len(data)):    
+        aux = data.iloc[n, :].isnull()
+        flag_to_add = True
+        for item in aux:
+            if item == True:
+                flag_to_add = False
+                break
+        if(flag_to_add):
+            yield data.iloc[n, :]
+
+def removeNegatives(x_data, y_data):
+    # Crear una lista para almacenar los índices de las filas con valores negativos
+    negative_rows = []
+    for i, row in y_data.iterrows():
+        # Revisar cada valor de la fila actual
+        for val in row:
+            # Si se encuentra un valor negativo, agregar el índice de la fila a la lista
+            if val < 0:
+                negative_rows.append(i)
+                break
+    # Eliminar las filas con valores negativos de ambos dataframes
+    x_data = x_data.drop(index=negative_rows)
+    y_data = y_data.drop(index=negative_rows)
+    return x_data, y_data
+
+def removeOvervalues(x_data, y_data):
+    # Crear una lista para almacenar los índices de las filas con valores negativos
+    overRows = []
+    for i, row in y_data.iterrows():
+        # Revisar cada valor de la fila actual
+        for val in row:
+            # Si se encuentra un valor negativo, agregar el índice de la fila a la lista
+            if val > 1.85:
+                overRows.append(i)
+                break
+    # Eliminar las filas con valores negativos de ambos dataframes
+    x_data = x_data.drop(index=overRows)
+    y_data = y_data.drop(index=overRows)
+    return x_data, y_data
+            
+# obtenemos los datos incompletos
+# recorremos las filas y determinamos su utilidad con una bandera, si su valor es False, la fila actual se guarda
+def getIncompleteRows(data):
+    for  n in range(len(data)):    
+        aux = data.iloc[n, :].isnull()
+        flag_to_add = True
+        for item in aux:
+            if item == True:
+                flag_to_add = False
+                break
+        if(flag_to_add is False):
+            yield data.iloc[n, :]
+            
+def saveUtilRows(data_to_walk):
+    data = []
+    for item in getUtilRows(data_to_walk):
+        data.append(item)
+    data = pd.DataFrame(data)
+    return data
+
+def saveIncompleteRows(data_to_walk):
+    data = []
+    for item in getIncompleteRows(data_to_walk):
+        data.append(item)
+    return pd.DataFrame(data)
+
+def splitXY(data, y_label_name):
+    x_data = data.drop([y_label_name], axis=1 )
+    y_data = pd.DataFrame(data[y_label_name])
+    return x_data, y_data
+
+
+def splitUtilIncompleteData(complete_data, y_label_name):
+    c_data = saveUtilRows(complete_data)
+    x_c_data, y_c_data = splitXY(c_data, y_label_name)
+    x_c_data. y_c_data = removeOvervalues(x_c_data, y_c_data)
+    i_data = saveIncompleteRows(complete_data)
+    x_i_data, y_i_data = splitXY(i_data, y_label_name)
+    x_c_data, y_c_data = removeNegatives(x_c_data, y_c_data)
+    
+    print(y_c_data)
+    return x_c_data, y_c_data, x_i_data, y_i_data
 
 data = pd.read_csv('./estaturas.csv')
 
-plt.plot(data['Edad'],data['Estatura'], 'ro')
+# x_complete_data, y_complete_data, x_incomplete_data, y_incomplete_data
+x_c_data, y_c_data, x_i_data, y_i_data = splitUtilIncompleteData(data, "Estatura")
+
+df = saveUtilRows(data)
+fig, axes = plt.subplots(nrows=1, ncols=3)
+
+df.plot(ax=axes[0], kind='scatter', y='Estatura', x='Peso', color='red')
+
+lrM = LinearRegression()
+lrM.fit(x_c_data, y_c_data)
+prediccion = lrM.predict(x_c_data)
+
+plt.subplot(1,3,2)
+
+plt.title('Prediccion 1 - sin limpieza')
+plt.xlabel('Peso')
+plt.ylabel('Estatura')
+plt.plot(x_c_data, prediccion)
+plt.plot(x_c_data, y_c_data, 'o')
+
+plt.subplot(1,3,3)
+
+plt.title('Prediccion 2 - con Limpieza')
+plt.plot(x_c_data, prediccion,'go')
+plt.xlabel('Peso')
+plt.ylabel('Estatura')
+plt.plot(x_c_data, prediccion)
+
 plt.show()
